92def3cf31071dff79f4d33a37bef616b21f30ff
skececi <sam.kececi@gmail.com>
2023-05-07 21:42:18 -0400

    working ;)

--- gpt.py
+++ gpt.py
@@ -4,9 +4,6 @@ import openai
 # Load your API key from an environment variable or secret management service
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-response = openai.Completion.create(model="text-davinci-003", prompt="Say this is a test", temperature=0, max_tokens=7)
-
-
 
 PROMPT_PREPEND = r"""
 you are a helpful CLI tool suggesting followup commands to run next. the following is the output of the process that just ran. tell me what happened in the output, and suggest 3 options for commands that could be run directly after this one (and a short comment - no longer than 25 words - on what each of your suggested commands will do).
@@ -17,11 +14,23 @@ output in the following format. DO NOT include any headers of prefacing text, ge
 2. `second command` : summary of SECOND OPTION
 3.  `third command` : summary of THIRD OPTION
 
-always refer to the output as "the previous command". i need to be able to parse this into code so follow the instructions precisely
+always refer to the output as "the previous command". i need to be able to parse this into code so follow the instructions precisely. 
+the three commands must be able to be executed right away without any additional input from the user. avoid commands with placeholders.
+if you must use a placeholder (avoid at all costs!), it must be between angle brackets <placeholder>. MAKE SURE Each placeholder is a unique string.
+However, AVOID PLACEHOLDERS AT ALL COSTS. DO NOT USE PLACEHOLDERS.
 
 Here is the output of the previous command:
 """
 
 
-def send_output_to_llm(stdout, stderr):
-    
+def send_output_to_llm(output: str):
+    print("Sending output to LLM...")
+    prompt = PROMPT_PREPEND + output
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo", messages=[{"role": "user", "content": prompt}]
+    )
+    print("LLM response: ")
+    print(completion)
+    print(completion["choices"][0]["message"]["content"].split("\n"))
+    return completion["choices"][0]["message"]["content"]
+
--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import os
-import sys 
 import subprocess
-
+import sys
 from pprint import pformat
+import re
+
+
+import inquirer
 
 from first_time_setup import write_to_config_file
 from gpt import send_output_to_llm
-
-
-
 
 LAST_CMD_OUTPUT_FILE = "/tmp/last_cmd"
 
@@ -19,28 +19,128 @@ def get_last_command():
         last_command = file.read().strip()
         print("Last command: " + last_command)
         return last_command
-    
 
-def run_last_command_and_get_output():
+
+def run_last_command_and_get_output() -> str:
+    skip_commands = ["python3", "oops", "fix_command"]
+
     last_command = get_last_command()
+    if last_command is None:
+        print("No last command found. Exiting.")
+        sys.exit(1)
+    # error handling to avoid recursion from skip commands. check if any of the skip command strings are in the last command
+    if any(skip_command in last_command for skip_command in skip_commands):
+        print("Skipping command: " + last_command)
+        sys.exit(0)
     # Run the command in a new subprocess and capture the output
-    result = subprocess.run(last_command, shell=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    return result
+    output = subprocess.run(
+        last_command,
+        shell=True,
+        text=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    print("OUTPUT: ")
+    print(output)
+    return output
+
+def extract_placeholders(input_string):
+    # Define a regular expression pattern to match placeholders between angle brackets
+    pattern = r'<(.*?)>'
+    # Use re.findall() to find all occurrences of the pattern in the input string
+    placeholders = re.findall(pattern, input_string)
+    # Return the list of extracted placeholders
+    return placeholders
+
+def prompt_and_substitute(input_string, placeholders):
+    # Create a dictionary to store the user's input for each placeholder
+    user_inputs = {}
+    # Prompt the user for input for each placeholder
+    for placeholder in placeholders:
+        user_input = input(f"Enter value for {placeholder}: ")
+        user_inputs[placeholder] = user_input
+    # Substitute the user's input back into the original string
+    for placeholder, user_input in user_inputs.items():
+        input_string = input_string.replace(f"<{placeholder}>", user_input)
+    return input_string
+
+
+def parse_response(llm_response: str):
+    """Parses the response from LLM into a summary and a list of suggested commands."""
+    split_response = llm_response.split("\n")
+    summary = split_response[0]
+    suggested_commands = split_response[1:]
+    commands_with_descriptors = []
+    for suggested_command in suggested_commands:
+        try:
+            print(suggested_command)
+            command = suggested_command.split(":")[0].split("`")[1]
+            print("command " + command)
+            command_descriptor = suggested_command.split(":")[1]
+            print("command_descriptor" + command_descriptor)
+            commands_with_descriptors.append(suggested_command)
+        except:
+            print("Error parsing command: " + suggested_command)
+            
+    return (summary, commands_with_descriptors)
+
+
+
+def get_user_selection(commands_with_descriptors):
+    # Define the list of commands
+
+    # Create a list prompt using inquirer
+    questions = [
+        inquirer.List(
+            'selected_command',
+            message='Select a command to run (ctrl+C to cancel):',
+            choices=commands_with_descriptors,
+        ),
+    ]
+
+    # Prompt the user and get the selected command
+    answers = inquirer.prompt(questions)
+    if answers is None:
+        print("No command selected. Exiting.")
+        sys.exit(1)
+
+    selected_command_with_descriptor = answers['selected_command']
+
+    # get the command itself
+    selected_command = selected_command_with_descriptor.split("`")[1]
+    print("Selected command: " + selected_command)
+
+    placeholders = extract_placeholders(selected_command)
+    # if placeholders is not empty
+    if placeholders:
+        substituted_string = prompt_and_substitute(selected_command, placeholders)
+    else:
+        substituted_string = selected_command
+
+    print(f'Running command: {substituted_string}')
+
+    # run the command in the current shell
+    os.system(substituted_string)
 
 
 def fix_command():
-    """Fixes previous command. Used when called without arguments."""
-    result = run_last_command_and_get_output()
-    print("RESULT: " + result)
-    llm_response = send_output_to_llm(result)
+    """Analyzes previous command."""
+    output = run_last_command_and_get_output()
+    output_str = output.__str__()
+
+    llm_response = send_output_to_llm(output_str)
+    summary, commands_with_descriptors = parse_response(llm_response)
     print(llm_response)
+    print("Summary of last command: " + summary)
+    print("\n")
+    get_user_selection(commands_with_descriptors)
+
 
 def entrypoint():
-    print("Hit main. Fixing your command...")    
+    print("Hit main. Fixing your command...")
     fix_command()
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     # write_to_config_file()
     entrypoint()
